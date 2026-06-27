5b839cb10192c875c4408fe9c3b3a66f8d2fff10
Pedr0cs <taving04@icloud.com>
2026-06-27 17:21:33 -0500

    Adiciona dicionarios e funcao para extrair valores unicos

--- livro.py
+++ livro.py
@@ -2,11 +2,20 @@ with open('livros.txt', 'r', encoding= 'utf-8') as arquivos:
     lista=[]
     for linha in arquivos:
         dados = linha.strip().split(',')
-        lista.append(dados)
+        dicionario = {
+    'titulo': dados[0].strip(), 'autor': dados[1].strip(), 'paginas': dados[2].strip(), 'genero': dados[3].strip()
+        }
+        lista.append(dicionario)
+              
+def repeticoes(livros, informacao):
+    lista_limpa= []
+    for dicionario in livros:
+        lista_limpa.append(dicionario[informacao])
+    return list(set(lista_limpa))
 
+titulos_unicos = repeticoes(lista, 'titulo')
+autores_unicos = repeticoes(lista, 'autor')
+generos_unicos = repeticoes(lista, 'genero')
 
-print(lista[0])
-print(lista[0][0])
-print(lista[0][1])
-print(lista[0][2])
-print(lista[0][3])
+for genero in generos_unicos:
+    print(genero)
