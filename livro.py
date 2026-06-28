e912f04e8ed0276ca08f1ba965f9fbb68345ecc4
Pedr0cs <taving04@icloud.com>
2026-06-28 15:48:58 -0500

    Adicionanando contagem de frequencia e ajustando funcao repeticoes para evitar duplicacao

--- livro.py
+++ livro.py
@@ -11,20 +11,29 @@ with open('livros.txt', 'r', encoding= 'utf-8') as arquivos:
             break
         lista.append(dicionario)
 
-              
+def com_rep(data, books):
+    lista_rep= []
+    for dicionario in data:
+        lista_rep.append(dicionario[books])
+    return (lista_rep)
+
+autores_frequentes = com_rep(lista, 'autor')
+generos_frequentes = com_rep(lista, 'genero')
+
 def repeticoes(livros, informacao):
-    lista_limpa= []
-    for dicionario in livros:
-        lista_limpa.append(dicionario[informacao])
-    return list(set(lista_limpa))
+    lista_com_repeticao = com_rep(livros, informacao)
+    return list(set(lista_com_repeticao))
 
 try: 
     titulos_unicos = repeticoes(lista, 'titulo')
     autores_unicos = repeticoes(lista, 'autor')
     generos_unicos = repeticoes(lista, 'genero')
 
-    for autor in autores_unicos:
-        print(autor)
-
+    for genero in generos_unicos:
+       print(genero, generos_frequentes.count(genero))
+    #for autor in autores_unicos:
+    #    print(autor, autores_frequentes.count(autor))
+    #for titulos in titulos_unicos:
+    #    print(titulos)
 except:
     print('Infelizmente ocorreu um erro. Tente novamente')
